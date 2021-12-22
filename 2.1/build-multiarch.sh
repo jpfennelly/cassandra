fa7f95edc7e09ee2906a3e6dc0c0c17bbebcdfaf
Jay Fennelly <jfennelly@commerce-architects.com>
2021-12-22 13:36:42 -0800

    Update latest 2.1 version (2.1.22) for multi-architecture builds and create 2.1.16 version needed for CB

---
+++ 2.1/build-multiarch.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# multi-architecture images have to be pushed to a registry, need to update with a registry here to use this
+#docker buildx build --push --platform linux/amd64,linux/arm64 -t cassandra:2.1.22 .
